5fe1fd36601cde29ea73016038e769fc5493b462
Jeremy Bakker <jeremybakker@jeremys-mbp.attlocal.net>
2017-01-26 22:19:06 -0600

    fixed git issues

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-Instructions
+# Lambda Expression Calculator
 
-Create an HTML file that has two input fields to accept the two numbers to perform operations on.
-Create 4 buttons to perform each of the basic mathematical functions (add, subtract, multiply, divide).
-In your JavaScript, put an event listener on each of the buttons.
-Copy the code below an implement a basic calculator.
-When the user performs one of the operations, output the result to another DOM element of your choice.
+## Setup
+
+These commands are a helpful quick start. You may choose to ignore them completely and create your own directory structure. If you choose to use this recommendation, just copy the commands below and paste. It doesn't matter what directory you are currently in.
+
+```bash
+mkdir -p ~/workspace/exercises/spa/calculator && cd $_
+touch index.html
+touch calculator.js
+```
+
+## Instructions
+
+1. Create an HTML file that has two input fields to accept the two numbers to perform operations on.
+1. Create 4 buttons to perform each of the basic mathematical functions (add, subtract, multiply, divide).
+1. In your JavaScript, put an event listener on each of the buttons.
+1. Copy the code below an implement a basic calculator.
+1. When the user performs one of the operations, output the result to another DOM element of your choice.
+
+```js
 /*
   Create a function that multiplies two numbers
   passed in as arguments. Return the product.
@@ -38,3 +52,5 @@ When the user performs one of the operations, output the result to another DOM e
 
   Return the value of the operation.
  */
+
+```
--- calculator.js
+++ calculator.js
@@ -1,59 +1,44 @@
-<<<<<<< HEAD
+
 // establish a global variable to hold the total
 var total;
 
-// add event listeners to each button
-=======
-// set a global variable for the total
-var total;
 
 // set event listeners on the buttons
->>>>>>> af322d976727c3b7052cd0e69520bd44dc47e79b
+
 document.getElementById("add").addEventListener("click", add);
 document.getElementById("subtract").addEventListener("click", subtract);
 document.getElementById("multiply").addEventListener("click", multiply);
 document.getElementById("divide").addEventListener("click", divide);
 document.getElementById("clear").addEventListener("click", clear);
 
-<<<<<<< HEAD
-// check whether the user has entered a valid number and alert if error
-=======
+
 // determine whether the user has entered valid numbers in both fields
->>>>>>> af322d976727c3b7052cd0e69520bd44dc47e79b
+
 function checkValidity() {
 	if (firstNumber == "" || secondNumber == "" || isNaN(firstNumber) || isNaN(secondNumber)) {
 		alert("Please fill both fields with appropriate values.");
 	}
 }
 
-<<<<<<< HEAD
-// change input to integers
-=======
+
 // change the input to an integer to allow for math operations
->>>>>>> af322d976727c3b7052cd0e69520bd44dc47e79b
+
 function parseValues() {
 	firstNumber = parseInt(document.getElementById("firstNumber").value);
 	secondNumber = parseInt(document.getElementById("secondNumber").value);
 }
 
-<<<<<<< HEAD
-// clear the window when the clear button is pressed
-=======
+
 // reset the window when the user clicks the "clear" button
->>>>>>> af322d976727c3b7052cd0e69520bd44dc47e79b
 function clear() {
 	window.location.reload();
 }
 
-<<<<<<< HEAD
-// each function below resets the solution field, grabs the input, parses them into numbers, checks their validity, 
-	// performs a math operation on them, puts the solution in the DOM, and resets the total to await the next
-	// calculation
 
-=======
+
 // perform the appropriate mathematical operation on the two numbers, beginning with clearing the solution field. 
 // this prevents the solutions from stacking left to right (e.g., 33333) for five separate 1 + 2 operations.
->>>>>>> af322d976727c3b7052cd0e69520bd44dc47e79b
+
 function add() {
 	document.getElementById("solution").innerHTML = "";
 	parseValues();
